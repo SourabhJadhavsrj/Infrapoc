4c29e66a9457cbeb816a8385985df9df5239cdf5
SourabhJadhavsrj <115058658+SourabhJadhavsrj@users.noreply.github.com>
2024-09-11 11:06:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+locals {
+  project_id = "ascendant-quest-400509"
+}
+
+resource "google_project_service" "compute_service" {
+  project = local.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "nginx-instance"
+  machine_type = var.linux_instance_type
+  tags = ["nginx-instance"]
+  boot_disk {
+    initialize_params {
+      image = var.cis-rhel-8-lvl-1
+    }
+}
+  network_interface {
+    network = "default"
+} 
+
+}
